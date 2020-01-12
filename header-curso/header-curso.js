3d110d3d31b5520e8470a81144164d19e8f09f04
m <marianagregorio@hotmail.com.br>
2020-01-12 10:41:01 -0300

    fotografia 2020

--- header-curso/header-curso.js
+++ header-curso/header-curso.js
@@ -1,15 +1,16 @@
-const headerCursoHtml = (nomeCurso) => {
+const headerCursoHtml = (nomeCurso, professores, backgroundImage) => {
+    // backgroundImage = "./img/amora_fotografia_bloco03-2020.jpg";
     return `
     <link rel="stylesheet" href="header-curso/header-curso.css" type="text/css" />
 
     <div id="tituloBloco03">
             <h1>Sobre o curso de ${nomeCurso}</h1>
         </div>
-        <div class="professorResponsavel">
-            <h4>Professores Responsáveis: Gustavo Názio e Wesley Gomes</h4>
+        <div class="professorResponsavel" style='background-image: url("${backgroundImage}");'>
+            <h4>${professores}</h4>
         </div>
      `
 }
 
 const headerCursoEl = document.querySelector('amora-header-curso');
-headerCursoEl.innerHTML = headerCursoHtml('Coregrafia Musical');
+headerCursoEl.innerHTML = headerCursoHtml(curso, professores, backgroundImage);
